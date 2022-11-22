974432a49e54de1d892dc322babbef963ade5982
shipra jain <sjain@calance.com>
2022-11-22 11:31:17 +0530

    commiting code for odata app

--- js/config/config.js
+++ js/config/config.js
@@ -4,6 +4,9 @@ app.service("mainConfig", [
       this.dataUrl = "https://fakestoreapi.com/products",
       this.editDeleteDataUrl = function (id) {
             return "https://fakestoreapi.com/products/" + id;
-        }
+        },
+       // this.odataV4Url = "https://services.odata.org/TripPinRESTierService/(S(uysxiwe1blb3dqplrneqg5bm))/people",
+        this.odataV3Url = "https://services.odata.org/V3/(S(uysxiwe1blb3dqplrneqg5bm))/OData/OData.svc",
+        this.jsonFormat = "$format=json"
     }
 ]);
